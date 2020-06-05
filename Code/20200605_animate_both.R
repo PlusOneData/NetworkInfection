81c71572582c34203116e2fe9ffd6ec6e88ae437
Ortiz <benjamin.a.ortiz@accenturefederal.com>
2020-06-05 02:04:03 -0400

    adjusted gif locations

--- Code/20200605_animate_both.R
+++ Code/20200605_animate_both.R
@@ -4,7 +4,6 @@ library(igraph)
 library(ggplot2)
 library(animation)
 
-graphIterations <- list(test0[[15]])
 
 drawSIRLines <- function(dta){
   ggplot(dta) +
@@ -26,7 +25,7 @@ dplyr::filter(stats2, time <= 1)
 gridAnimPrep <- purrr::map(seq_along(test2), function(i){
   print(i)
   
-  plot(test2[[i]],
+  set.seed(4321); plot(test2[[i]],
        vertex.label = '', 
        vertex.size = 3,
        asp = 0)
@@ -38,24 +37,24 @@ gridAnimPrep <- purrr::map(seq_along(test2), function(i){
   b <- ggplotGrob(b)
   
   grid.newpage()
-  pushViewport(viewport(x = .75, y = .5, width = .6, height = .6))
+  # grid.draw(textGrob(paste0("T = ", i), x = .5, y = .2))
+  pushViewport(viewport(x = .75, y = .7, width = .6, height = .6))
   grid.draw(a)
   popViewport()
-  pushViewport(viewport(x = .25, y = .5, width = .5, height = .5))
+  pushViewport(viewport(x = .25, y = .7, width = .5, height = .5))
   grid.draw(b)
   popViewport()
-  grid.draw(textGrob(paste0("T = ", i), x = .5, y = .2))
   finalChart <- grid.grab()
-  grid.draw(finalChart)
+  # grid.draw(finalChart)
 })
 
 saveGIF({
-  ani.options(ani.width = 600,
-              ani.width = 600,
-              interval = .3)
+  ani.options(ani.width = 1000,
+              ani.width = 1000,
+              interval = .4)
 
   purrr::map(seq_along(gridAnimPrep), function(i){
     grid.newpage()
     grid.draw(gridAnimPrep[[i]])
   })
-}, movie.name = '20200605_small_world.gif')
+}, movie.name = '20200605_small_world_upper.gif')
