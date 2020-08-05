bb6daca4a4224121c451532990165a9079f89115
Collin <cjschwantesll@gmail.com>
2020-08-05 14:44:00 -0400

    changed testing to a pure testing module, added false positive and false negative

--- Code/testingModule.R
+++ Code/testingModule.R
@@ -20,21 +20,28 @@
 #' 1 is added to the \code{$testCounter} property if a an infection was previously detected.
 #' If \code{$testCounter} is equal to or greater than \code{testDelay} and \code{$detected} is TRUE, the node becomes recovered.
 #'
-#' @field testDelay
-#' @field testFrequency
+#' @field testDelay Number time steps to test results
+#' @field testFrequency Number time steps between testing
+#' @field falseNegRate frequency that a person with covid tests negative
+#' @field falsePosRate frequency that a person without covid tests positive
 #' @export default_testing
 default_testing <- setRefClass(
   "testing",
   fields = list(testDelay="numeric",
-                testFrequency="numeric"
+                testFrequency="numeric",
+                falseNegRate="numeric",
+                falsePosRate="numeric"
   ),
   methods = list(
     init = function(g) {
       "Check whether or not nodes are infected"
-      igraph::V(g)$detected <- ifelse(igraph::V(g)$infected ==1, 1, 0)
+      igraph::V(g)$detected <- ifelse(igraph::V(g)$infected ==1, rbinom(1,1,(1-falseNegRate)), rbinom(1,1,0+falsePosRate))
       "Set test delay counter"
       igraph::V(g)$testCounter <- 0
+      "Set outbreak day counter for testing interval"
       igraph::V(g)$outbreakDay <- 0
+      "Set Reported value"
+      igraph::V(g)$reported <- 0
       return(g)
     },
     donext = function(g) {
@@ -45,15 +52,15 @@ default_testing <- setRefClass(
       "Test for disease at certain intervals"
       if((max(igraph::V(g)$outbreakDay) %% testFrequency) == 0){ 
       "Test whether or not nodes are infected"
-      igraph::V(g)$detected <- ifelse(igraph::V(g)$infected ==1, 1, 0)
+      igraph::V(g)$detected <- ifelse(igraph::V(g)$infected ==1, rbinom(1,1,(1-falseNegRate)), rbinom(1,1,0+falsePosRate))
       }
       
       "increment or reset testCounter for detected cases"
       igraph::V(g)$testCounter <- ifelse(igraph::V(g)$detected == 1,igraph::V(g)$testCounter +1, 0)
       
       "Check testCounter and Remove detected nodes"
-      igraph::V(g)$infected <- ifelse(igraph::V(g)$testCounter == testDelay,2, igraph::V(g)$infected)
-      igraph::V(g)$color <- ifelse(igraph::V(g)$testCounter == testDelay,"green", igraph::V(g)$color)
+      igraph::V(g)$reported <- ifelse(igraph::V(g)$testCounter == testDelay,1, igraph::V(g)$infected)
+      igraph::V(g)$color <- ifelse(igraph::V(g)$testCounter == testDelay,"yellow", igraph::V(g)$color)
       
       return(g)
     }
