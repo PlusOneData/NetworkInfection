e1c4042354c5c14b37c61a5079b36eb9f6e5d608
Collin <cjschwantesll@gmail.com>
2020-11-03 19:05:57 -0500

    changed the testingfrequecny

--- Code/simulation_testing.R
+++ Code/simulation_testing.R
@@ -66,7 +66,7 @@ gmma <- 14
 covid_ppe <- default_ppe(faceCovering = 0.9, eyeProtection = 0.9, distancing = .85, compliance = .95)
 covid_di <- ppe_density_infect(init_num = 3, transRate = prob.infect)
 covid_dr <- default_recover(max_recovery_time = 20)
-covid_dt <- default_testing(testDelay = 1, testFrequency = 3, falseNegRate = 0.03, falsePosRate = 0.001, propTested = 1)
+covid_dt <- default_testing(testDelay = 2, testFrequency = 3, falseNegRate = 0.03, falsePosRate = 0.001, propTested = 1)
 covid_lv <- default_leave(leaveDuration = 10, max_recovery_time = 20)
 covid_model_density <- infection_model(components = list(covid_ppe,covid_di, covid_dr,covid_dt, covid_lv))
 
