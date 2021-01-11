266b458cd10efe8920da80c9cf05f466a9f18e9f
Collin <cjschwantesll@gmail.com>
2021-01-10 20:22:38 -0500

    added to donext function

--- Code/spatial_trans_module.R
+++ Code/spatial_trans_module.R
@@ -57,7 +57,7 @@ spatial_transmission <- setRefClass(
               ## get probInf given infProbReduction
               probInf <- igraph::V(g)[infected == 0]$infProbReduction * envTransRate
               
-              ## get 
+              ## get inf status
               infStatus <- rbinom(1,1,min(probInf,1))
               
               igraph::V(g)[infected == 0][infStatus]$infected <- 1
@@ -67,16 +67,48 @@ spatial_transmission <- setRefClass(
             }
           }
         }
-        
-        
-       
       
       return(g)
     },
     donext = function(g) {
-      "Update infectiousness profile"
-      maxInf <- max(relInfFunc(x = 0:max_recovery_time))
-      igraph::V(g)$relInf <- relInfFunc(x = igraph::V(g)$counter)/maxInf
+
+      "simulate spatial infections and movements"
+      rooms$infStatus <- 0
+      rooms$virusConc <- 0 
+      
+      for(room in rooms$name){
+        df <- rooms %>% 
+          filter(name == room)
+        
+        for(i in 1:32){
+          # need to find where each node is at time step 1
+          igraph::V(g)$currentLoc <- igraph::V(g)$schedule %>% 
+            stringr::str_sub(.,start = i,end = i)
+          
+          nodes <- igraph::V(g)[currentLoc == room]
+          
+          #how much sars-cov-2 is emitted into the room
+          roomCon <- (sum(nodes$relInf*conRate))/df$volume
+          
+          #update virus concentration in room and remove 
+          df$virusConc <- (df$virusConc + roomCon)*deconRate 
+          
+          if(df$virusConc >= infConc){ 
+            
+            ## get probInf given infProbReduction
+            probInf <- igraph::V(g)[infected == 0]$infProbReduction * envTransRate
+            
+            ## get inf status
+            infStatus <- rbinom(1,1,min(probInf,1))
+            
+            igraph::V(g)[infected == 0][infStatus]$infected <- 1
+            igraph::V(g)[infected == 1]$color <- "red"
+            
+            
+          }
+        }
+      }
+      
       return(g)
     }
   )
