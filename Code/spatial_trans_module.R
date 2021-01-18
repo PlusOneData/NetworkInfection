51eee441652f527075866284c197bb2e6d848b34
Collin <cjschwantesll@gmail.com>
2021-01-18 12:20:51 -0500

    added description to module

--- Code/spatial_trans_module.R
+++ Code/spatial_trans_module.R
@@ -4,6 +4,43 @@
 #' a given rate and decontaminated at a given rate. Individuals can be infected after spending
 #' a certain amount of time in a room with a certain level of contamination.
 #'
+#' Emission rate  infection risk based on work by Buonanno et al. 
+#' \url{https://www.sciencedirect.com/science/article/pii/S0160412020312800}
+#' 
+#' 
+#' Emission Rate
+#' 
+#' \deqn{ER_{q,j} = c_v*c_i*IR*\sum_{i=1}^4(N_{i,j}*V_i)}
+#' 
+#' \eqn{ER_{q,j}} = quanta emission rate
+#' \eqn{c_v} = viral load in sputum
+#' \eqn{c_i} = conversion factor between quanta and infectious dose viral rna copies
+#' \eqn{IR} = inhalation rate
+#' \eqn{\sum_{i=1}^4(N_{i,j}*V_i)} = sum of different expiration types
+#'
+#' In this module, we have made simplifying assumptions about expiration types and 
+#' inhalation rates - we assume they are homogeneous for all individuals. 
+#'
+#'Viral concentration
+#'
+#'\deqn{n(t) = \frac{ER_q*I}{IVRR*V}+(n_o+frac{ER_q*I}{IVRR})*frac{e^{IVRR*t}}{V}}
+#'
+#'\eqn{IVRR} = infectious viral removal rate (ventilation + decay + deposition)
+#'\eqn{I} = number of infecitous individuals
+#'\eqn{ER_q} = quanta emission rate
+#'\eqn{V} = volume of space
+#'\eqn{n_0} = initial concentration of virus
+#'
+#'Infection Risk 
+#'
+#'A funciton of exposure time
+#'
+#'\deqn{R = (1-e^{-IR\int_0^Tn(t)dt)}
+#'
+#'\eqn{IR} = inhalation rate
+#'\eqn{T} = total time of exposure
+#'
+#'
 #' @field rooms Dataframe of rooms with name and volume attributes
 #' @field infConc Numeric, concentration where room becomes infectious
 #' @field conRate Numeric, rate of contamination by infectious individuals
