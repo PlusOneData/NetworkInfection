1113e891281e0a749e8aea3c065b8158d41d2dde
Collin <cjschwantesll@gmail.com>
2021-01-19 13:54:18 -0500

    modified init and donext to include spatial trans method

--- Code/spatial_trans_module.R
+++ Code/spatial_trans_module.R
@@ -78,44 +78,7 @@ spat_tran <- setRefClass(
       rooms$infStatus <- 0
       rooms$virusConc <- 0 
       
-        for(room in rooms$name){
-          df <- rooms %>% 
-            filter(name == room)
-          
-          # think about making a room class to handle unique room actions to 
-          # compute who gets infected. Would initialize a group of rooms. At 
-          # each time step individuals would be assigned and room object would
-          # tell us who got infected. 
-          
-          for(i in 1:32){
-            # need to find where each node is at time step 1
-            igraph::V(g)$currentLoc <- igraph::V(g)$schedule %>% 
-              stringr::str_sub(.,start = i,end = i)
-            
-            nodes <- igraph::V(g)[currentLoc == room]
-            
-            #how much sars-cov-2 is emitted into the room
-            roomCon <- (sum(nodes$relInf*conRate))/df$volume
-            
-            #update virus concentration in room and remove 
-            df$virusConc <- (df$virusConc + roomCon)*deconRate # additional terms vent vs natural? 
-            
-            if(df$virusConc >= infConc){ 
-              
-              ## get probInf given infProbReduction
-              probInf <- igraph::V(g)[infected == 0]$infProbReduction * envTransRate #secondary attack rate from aerosols
-              
-              ## get inf status
-              infStatus <- rbinom(1,1,min(probInf,1))
-              
-              igraph::V(g)[infected == 0][infStatus]$infected <- 1
-              igraph::V(g)[infected == 1]$color <- "red"
-              
-              # room x time matrix
-              # need to be compute schedules on mass
-            }
-          }
-        }
+      g <- spatialTrans(g,rooms)
       
       return(g)
     },
@@ -125,42 +88,11 @@ spat_tran <- setRefClass(
       rooms$infStatus <- 0
       rooms$virusConc <- 0 
       
-      for(room in rooms$name){
-        df <- rooms %>% 
-          filter(name == room)
-        
-        for(i in 1:32){
-          # need to find where each node is at time step 1
-          igraph::V(g)$currentLoc <- igraph::V(g)$schedule %>% 
-            stringr::str_sub(.,start = i,end = i)
-          
-          nodes <- igraph::V(g)[currentLoc == room]
-          
-          #how much sars-cov-2 is emitted into the room
-          roomCon <- (sum(nodes$relInf*conRate))/df$volume
-          
-          #update virus concentration in room and remove 
-          df$virusConc <- (df$virusConc + roomCon)*deconRate 
-          
-          if(df$virusConc >= infConc){ 
-            
-            ## get probInf given infProbReduction
-            probInf <- igraph::V(g)[infected == 0]$infProbReduction * envTransRate
-            
-            ## get inf status
-            infStatus <- rbinom(1,1,min(probInf,1))
-            
-            igraph::V(g)[infected == 0][infStatus]$infected <- 1
-            igraph::V(g)[infected == 1]$color <- "red"
-            
-            
-          }
-        }
-      }
+      g <- spatialTrans(g,rooms)
       
       return(g)
     },
-    spatialTrans = function(g){
+    spatialTrans = function(g,rooms){
       ## want to get location at time point 1 
       for(i in 1:32){
         # need to find where each node is at time step 1
